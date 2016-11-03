a68a39dc2ed3da6e5c6737e65365a4df5d032c8a
Rainboy <597872644@qq.com>
2016-11-03 22:45:28 +0800

    chn 一些

--- test/test.js
+++ test/test.js
@@ -6,6 +6,8 @@ var config  = require('../config.js')
 var fs      =require('fs')
 
 
+var judge_address = 'http://127.0.0.1'
+
 var pathFn = require('path');
 var join = pathFn.join;
 var ext = pathFn.extname;
@@ -16,36 +18,23 @@ function out_cotent(file_path){
     return fs.readFileSync(file_path,{encoding:'utf8'});
 }
 
-//describe('文件上传测试',function(){
+describe('文件上传测试',function(){
 
-    //var uid = shortid.generate();
+    var uid = shortid.generate();
 
-    //it('zip上传',function(done){
-        //postFile(uid,join(n_path,'test.zip'),'zip','http://127.0.0.1:'+config.port+'/file')
-            //.then(function(result){
-                //result.state.should.eql('success');
-                //result.uid.should.eql(uid);
+    it('zip上传',function(done){
+        postFile(uid,join(n_path,'test.zip'),'.zip',judge_address+':'+config.port+'/file')
+            .then(function(result){
+                result.state.should.eql('success');
+                result.uid.should.eql(uid);
                 //var out = 'hello world!\nhello world!\n';
                 //out.should.eql( out_cotent( join(config.prefix,uid,'1.in')));
                 //out = 'hello Rainboy!\n';
                 //out.should.eql( out_cotent( join(config.prefix,uid,'1.out')));
-                //done();
-            //})
-    //})
-    
-    //it('代码文件上传',function(done){
-        //var codepath =join(n_path,'test.cpp');
-        //var type = ext(codepath)
-        //postFile(uid,codepath,type,'http://127.0.0.1:'+config.port+'/file')
-            //.then(function(result){
-                //result.state.should.eql('success');
-                //result.uid.should.eql(uid);
-                //var out = out_cotent(join(n_path,'test.cpp'));
-                //out.should.eql( out_cotent( join(config.prefix,uid,'main.cpp')));
-                //done();
-            //})
-    //})
-//})
+                done();
+            })
+    })
+})
 
 describe('简单代码评测测试',function(){
     var codepath =join(n_path,'cpp','1','hello.cpp');
@@ -54,7 +43,7 @@ describe('简单代码评测测试',function(){
     var t_config =    JSON.parse( fs.readFileSync(configPath));
     var uid= shortid.generate();
     it('zip上传',function(done){
-        postFile(uid,datazip,'.zip','http://127.0.0.1:'+config.port+'/tmpfile')
+        postFile(uid,datazip,'.zip',judge_address+':'+config.port+'/tmpfile')
             .then(function(result){
                 result.state.should.eql('success');
                 result.uid.should.eql(uid);
@@ -74,7 +63,7 @@ describe('简单代码评测测试',function(){
             max_memory:t_config.max_memory,
             output:false
         }
-        request.post({url:'http://127.0.0.1:'+config.port+'/judge',form:__config,json:true},
+        request.post({url:judge_address+':'+config.port+'/judge',form:__config,json:true},
             function(err,res,body){
                 console.log(body)
                 done()
@@ -82,39 +71,39 @@ describe('简单代码评测测试',function(){
     })
 })
 
-describe('代码评测测试',function(){
+//describe('代码评测测试',function(){
 
-    var codepath =join(n_path,'cpp','2','aplusb.cpp');
-    var datazip=join(n_path,'cpp','2','data.zip');
-    var configPath = join(n_path,'cpp','2','config.json');
-    var t_config =    JSON.parse( fs.readFileSync(configPath));
-    var uid= shortid.generate();
+    //var codepath =join(n_path,'cpp','2','aplusb.cpp');
+    //var datazip=join(n_path,'cpp','2','data.zip');
+    //var configPath = join(n_path,'cpp','2','config.json');
+    //var t_config =    JSON.parse( fs.readFileSync(configPath));
+    //var uid= shortid.generate();
 
-    it('zip上传',function(done){
-        postFile(uid,datazip,'.zip','http://127.0.0.1:'+config.port+'/tmpfile')
-            .then(function(result){
-                result.state.should.eql('success');
-                result.uid.should.eql(uid);
-                done();
-            })
-    })
+    //it('zip上传',function(done){
+        //postFile(uid,datazip,'.zip','http://127.0.0.1:'+config.port+'/tmpfile')
+            //.then(function(result){
+                //result.state.should.eql('success');
+                //result.uid.should.eql(uid);
+                //done();
+            //})
+    //})
 
-    it('a+b编译测试',function(done){
-        this.timeout(15000)
-        var src = fs.readFileSync(codepath);
-        var __config = {
-            src:src,
-            language:'cpp',
-            uid:uid,
-            tmp_uid:uid,
-            max_cpu_time:t_config.max_cpu_time,
-            max_memory:t_config.max_memory,
-            output:false
-        }
-        request.post({url:'http://127.0.0.1:'+config.port+'/judge',form:__config,json:true},
-            function(err,res,body){
-                console.log(body)
-                done()
-            })
-    })
-})
+    //it('a+b编译测试',function(done){
+        //this.timeout(15000)
+        //var src = fs.readFileSync(codepath);
+        //var __config = {
+            //src:src,
+            //language:'cpp',
+            //uid:uid,
+            //tmp_uid:uid,
+            //max_cpu_time:t_config.max_cpu_time,
+            //max_memory:t_config.max_memory,
+            //output:false
+        //}
+        //request.post({url:'http://127.0.0.1:'+config.port+'/judge',form:__config,json:true},
+            //function(err,res,body){
+                //console.log(body)
+                //done()
+            //})
+    //})
+//})
